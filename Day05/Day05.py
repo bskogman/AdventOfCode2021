a26b94f0be03d6af6bc859660d7782c83267edbb
bskogman <bskogman1@outlook.com>
2021-12-05 20:41:45 -0600

    Cleaned up Day 5 pt1

--- Day05/Day05.py
+++ Day05/Day05.py
@@ -16,6 +16,7 @@ for line in file:
 
 # get height and width
 gridpt1 = []
+gridpt2 = []
 bigx = 0
 bigy = 0
 for pts in line_ends:
@@ -24,42 +25,30 @@ for pts in line_ends:
             bigx = int(pt[0])
         if int(pt[1]) > bigy:
             bigy = int(pt[1])
-# print(bigx)
-# print(bigy)
+
 empty_row = []
 for x in range(0,bigx+1):
     empty_row.append(0)
 for y in range(0,bigy+1):
     gridpt1.append(empty_row.copy())
-# print(len(gridpt1))
-# print(len(gridpt1[0]))
-# print(len(line_ends))
+    gridpt2.append(empty_row.copy())
+
 # fill grid with lines for part 1 (only vertical/horizontal)
 for ends in line_ends:
     if (ends[0][0] == ends[1][0]):
         # draw horizontal line on grid
-        # print("matches on x")
         x1 = int(ends[0][0])
         x2 = int(ends[1][0])
         y1 = min(int(ends[0][1]),int(ends[1][1]))
         y2 = max(int(ends[0][1]),int(ends[1][1]))
-        # print(x1)
-        # print(x2)
-        # print(y1)
-        # print(y2)
         for ypts in range(y1,y2+1):
             gridpt1[ypts][x1] += 1
     elif (ends[0][1] == ends[1][1]):
         # draw vertical line on grid
-        # print("matches on y")
         y1 = int(ends[0][1])
         y2 = int(ends[1][1])
         x1 = min(int(ends[0][0]),int(ends[1][0]))
         x2 = max(int(ends[0][0]),int(ends[1][0]))
-        # print(x1)
-        # print(x2)
-        # print(y1)
-        # print(y2)
         for xpts in range(x1,x2+1):
             gridpt1[y1][xpts] += 1
 
@@ -74,5 +63,3 @@ def findoverlaps(filled_grid):
 pt1overlaps = findoverlaps(gridpt1)
 print("Answer for Pt 1: " + str(pt1overlaps))
 
-# print(line_ends)
-# print(gridpt1)
