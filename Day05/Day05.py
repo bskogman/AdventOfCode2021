5470698de4ef218ba2131df24b644b582ab75078
bskogman <bskogman1@outlook.com>
2021-12-05 20:39:05 -0600

    Day 5 pt 1 finished

--- Day05/Day05.py
+++ Day05/Day05.py
@@ -1,7 +1,8 @@
-with open('Day05\\test.txt') as f:
-# with open('Day05\\input.txt') as f:
+# with open('Day05\\test.txt') as f:
+with open('Day05\\input.txt') as f:
     file = f.read().split('\n')
 
+# file = file[40:50]
 # parse coordinates
 line_ends = []
 for line in file:
@@ -23,13 +24,16 @@ for pts in line_ends:
             bigx = int(pt[0])
         if int(pt[1]) > bigy:
             bigy = int(pt[1])
-
+# print(bigx)
+# print(bigy)
 empty_row = []
 for x in range(0,bigx+1):
     empty_row.append(0)
 for y in range(0,bigy+1):
     gridpt1.append(empty_row.copy())
-
+# print(len(gridpt1))
+# print(len(gridpt1[0]))
+# print(len(line_ends))
 # fill grid with lines for part 1 (only vertical/horizontal)
 for ends in line_ends:
     if (ends[0][0] == ends[1][0]):
@@ -37,8 +41,8 @@ for ends in line_ends:
         # print("matches on x")
         x1 = int(ends[0][0])
         x2 = int(ends[1][0])
-        y1 = int(min(ends[0][1],ends[1][1]))
-        y2 = int(max(ends[0][1],ends[1][1]))
+        y1 = min(int(ends[0][1]),int(ends[1][1]))
+        y2 = max(int(ends[0][1]),int(ends[1][1]))
         # print(x1)
         # print(x2)
         # print(y1)
@@ -50,8 +54,8 @@ for ends in line_ends:
         # print("matches on y")
         y1 = int(ends[0][1])
         y2 = int(ends[1][1])
-        x1 = int(min(ends[0][0],ends[1][0]))
-        x2 = int(max(ends[0][0],ends[1][0]))
+        x1 = min(int(ends[0][0]),int(ends[1][0]))
+        x2 = max(int(ends[0][0]),int(ends[1][0]))
         # print(x1)
         # print(x2)
         # print(y1)
@@ -68,7 +72,6 @@ def findoverlaps(filled_grid):
     return cum_sum
     
 pt1overlaps = findoverlaps(gridpt1)
-
 print("Answer for Pt 1: " + str(pt1overlaps))
 
 # print(line_ends)
